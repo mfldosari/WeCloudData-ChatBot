d5e454fac115cc69e8b2441e9098e81bc28dbee4
Maldosari <aldosari.mkj@gmail.com>
2025-02-25 11:06:49 +0000

    updated chat website (forontend) and added comments to explain the code

--- chatbot_streamlit.py
+++ chatbot_streamlit.py
@@ -14,17 +14,17 @@ from chatbot_request import get_openai_response
 if 'username' not in st.session_state:
     st.session_state.username = 'User'
     
-# Initializing session state for username if it doesn't exist
+# Initializing session state for chat name if it doesn't exist
 if 'current_chat_name' not in st.session_state:
     st.session_state.current_chat_name = 'untitled chat'
 
 # Initializing session state for bot status if it doesn't exist
 if 'botStatus' not in st.session_state:
-    st.session_state.botStatus = ['normal']
+    st.session_state.botStatus = 'normal'
     
 # Initializing session state for bot status if it doesn't exist
 if 'useravatar' not in st.session_state:
-    st.session_state.useravatar = ['defult']
+    st.session_state.useravatar = 'defult'
      
 
 # Helper function to convert image to base64 (to embed it in HTML)
@@ -37,7 +37,7 @@ def get_image_base64(img_path):
 # Function to display chat messages with avatars
 def display_chat_message(message, avatar_img_path):
     avatar_base64 = get_image_base64(avatar_img_path)
-    # Displaing user message with avatar
+    # Displaing user and chatbot message with avatar
     st.markdown(f"""
         <div style="display: flex; align-items: center;">
             <img src="data:image/png;base64,{avatar_base64}" width="40" height="40" style="border-radius: 50%; margin-right: 10px;">
@@ -67,7 +67,7 @@ def avatar_updater(role, post='normal'):
     return path
 
 bot_response_time = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-# MidSection function to display the chat and handle user input
+# MidSection function to display the chat area and handle user input
 def MidSection():
     # Initializing session state for chat history if it doesn't exist
     if 'chat_history' not in st.session_state:
@@ -87,15 +87,15 @@ def MidSection():
 
         try:
             # try and expect block to handle errors and success responses
-            st.session_state.botStatus[0] = 'normal'
-            # call the openai and plug the usermsg to get the user reponse
+            st.session_state.botStatus = 'normal'
+            # call the openai and plug the chat_history to get the chatbot reponse
             chatbot_response = get_openai_response(st.session_state.chat_history)
             # Saving the bot's response to the chat history after the delay
             st.session_state.chat_history.append({"role": "assistant", "content": chatbot_response, "time":bot_response_time})
             
         except Exception as e:
             # Show a notification of the error and notify the user. (e.g api not givin or unaccessable api)
-            st.session_state.botStatus[0] = 'error'
+            st.session_state.botStatus = 'error'
             st.toast("500: Failed to establish a new connection")
             chatbot_response = f"Sorry {st.session_state.username}, but something went wrong. Please try again later."
             st.session_state.chat_history.append({"role": "assistant", "content": chatbot_response, "time":bot_response_time})
@@ -118,47 +118,46 @@ def MidSection():
 MidSection()
 # Side Bar section:
 def button_ops_section():
-    #creating some elements such as title, caption, checkboxes and a button
+    #creating some elements such as title, caption, checkboxes and a buttons
     st.sidebar.title("Chat Options:")
     
     # Creating columns to display buttons horizontally
     col1, col2, col3 = st.sidebar.columns(3)
-
+    # delete button for deleting a current chat or stored chats
     with col1:
         delete_chat = st.button(':material/delete:')
-
+    #add new button, to add a new chat and store it if needed
     with col2:
         add_new_chat = st.button(':material/add:')
-
+    # loading old chats button to load chats that were stored in the storage
     with col3:
-        load_prev_chat = st.button(':material/cloud_download:')
+        load_prev_chat = st.button(':material/search:')
     
-    
+    # section to rename and name the current chat
     st.sidebar.caption("Current Chat Name:")
     name_input = st.sidebar.chat_input(f"{st.session_state.current_chat_name}")
-
+    # section to upload pdf
     st.sidebar.caption("Upload PDF file")
-    
     button_upload = st.sidebar.button(':material/file_upload: Upload')
-    
+    # section to chnage gendre (not necessary and could be deleted, if decided to delete make sure to delete its related functio too)
     st.sidebar.caption("I am a:")
     selection_boy = st.sidebar.checkbox(":material/male: Male", key='boy_echbox')
     selection_girl = st.sidebar.checkbox(":material/female: Female", key='girl_echbox')
 
     #if condition block to handle clikcing and entering a name
     if selection_boy and selection_girl:
-       st.session_state.useravatar[0] = 'defult'
+       st.session_state.useravatar = 'defult'
        st.rerun()
     if selection_boy:
-        st.session_state.useravatar[0] = 'boy'
+        st.session_state.useravatar = 'boy'
         st.rerun()
     if selection_girl:
-        st.session_state.useravatar[0] = 'girl'
+        st.session_state.useravatar = 'girl'
         st.rerun()
     if name_input:
         st.session_state.current_chat_name = name_input
         with st.spinner('renaming new chat..'):
-            time.sleep(1)  # Simulate a long task
+            time.sleep(1)  
         st.success(f"Good News, Chat named as ({name_input}) successfully..")
         st.rerun()
  
