fd0ab3fe6e42f4b018658ae6e8ed316e69d85126
Mohammed Aldosari <91144751+mfldosari@users.noreply.github.com>
2025-03-16 09:48:38 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -20,7 +20,7 @@ sudo -u azureuser /home/azureuser/env/bin/pip install --upgrade pip
 sudo -u azureuser /home/azureuser/env/bin/pip install -r "$APP_DIR/requirements.txt"
 
 # Restart the service
-sudo systemctl restart backend.service
-sudo systemctl restart forntend.service
+sudo systemctl restart chatbot.service
+
 echo "Applications update completed!"
 
