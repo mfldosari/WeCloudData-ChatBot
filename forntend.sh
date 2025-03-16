2c526504f940f1d993c33e2e6b144abf726c8a15
Mohammed Aldosari <91144751+mfldosari@users.noreply.github.com>
2025-03-16 07:06:23 +0000

    Create forntend.sh

---
+++ forntend.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Color functions
+info() { echo -e "\033[34m$1\033[0m"; }  # Blue
+success() { echo -e "\033[32m$1\033[0m"; }  # Green
+
+source /home/azureuser/env/bin/activate
+
+CURRENT_DIR=$(pwd)
+LOGS_DIR="$CURRENT_DIR/logs"
+CHATBOT_SCRIPT="$CURRENT_DIR/chatbot.py"
+
+
+# Ensure logs directory exists
+mkdir -p "$LOGS_DIR"
+info "Logs will be stored in: $LOGS_DIR"
+
+
+# Start Streamlit chatbot
+info "Starting Streamlit chatbot..."
+streamlit run "$CHATBOT_SCRIPT" --server.address $SERVER_IP --server.port 8502 > "$LOGS_DIR/streamlit.log" 2>&1 &  # Run in background
+CHATBOT_PID=$!
+success "Streamlit chatbot started at: http://$SERVER_IP:8502"
+
+# Prevent the script from exiting
+tail -f /dev/null
