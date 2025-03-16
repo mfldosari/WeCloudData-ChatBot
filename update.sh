1ffa555975184a957172c792a17d70f119afc771
Mohammed Aldosari <91144751+mfldosari@users.noreply.github.com>
2025-03-16 07:08:28 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -20,7 +20,7 @@ sudo -u azureuser /home/azureuser/env/bin/pip install --upgrade pip
 sudo -u azureuser /home/azureuser/env/bin/pip install -r "$APP_DIR/requirements.txt"
 
 # Restart the service
-sudo systemctl restart all-services.service
-
+sudo systemctl restart backend.service
+sudo systemctl restart forntend.service
 echo "Applications update completed!"
 
