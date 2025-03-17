6c0804b9801d4fe7dbbe9ee10c2c0390bd40c7bb
Ubuntu <azureuser@chatbot-vm.eul5nmor5b5utk35xej5p0ifyd.bx.internal.cloudapp.net>
2025-03-17 19:33:58 +0000

    added .services files to be retsarted

--- update.sh
+++ update.sh
@@ -20,7 +20,7 @@ sudo -u azureuser /home/azureuser/env/bin/pip install --upgrade pip
 sudo -u azureuser /home/azureuser/env/bin/pip install -r "$APP_DIR/requirements.txt"
 
 # Restart the service
-sudo systemctl restart chatbot.service
-
+sudo systemctl restart uvicorn.service
+sudo systemctl restart streamlit.service
 echo "Applications update completed!"
 
