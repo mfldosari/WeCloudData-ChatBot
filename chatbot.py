44434d3ae1f45d6a905c8fa20c366bca19a7f3de
jax wood <jax.wood.m@gmail.com>
2025-04-02 03:29:15 +0300

    remove test

--- chatbot.py
+++ chatbot.py
@@ -172,7 +172,6 @@ with st.sidebar:
 # ------------------------------
 # Main Content: Chat Interface with Avatars
 # ------------------------------
-st.header("Chatbot Application test")
 st.header("Chatbot Application")
 if st.session_state["current_chat"]:
     chat_id = st.session_state["current_chat"]
